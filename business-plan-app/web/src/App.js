f4ab37357b6f750f2ba5e6841e022de2d7932006
JoshS831 <segobiajoshua@outlook.com>
2025-03-29 22:22:48 +0000

    Integrate components into App.js and update index.html

--- business-plan-app/web/src/App.js
+++ business-plan-app/web/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { BusinessPlanForm, BusinessPlanList } from './components/BusinessPlanCreation';
-// Import other components as needed
-import './styles.css';
+import LegalBusinessFormation from './components/LegalBusinessFormation/LegalBusinessFormation';
+import EINNumber from './components/EINNumber/EINNumber';
+import BusinessBanking from './components/BusinessBanking/BusinessBanking';
+import VirtualOffice from './components/VirtualOffice/VirtualOffice';
+import BusinessPhoneNumber from './components/BusinessPhoneNumber/BusinessPhoneNumber';
+import BusinessWebsite from './components/BusinessWebsite/BusinessWebsite';
+import DunsNumber from './components/DunsNumber/DunsNumber';
+import VendorCredit from './components/VendorCredit/VendorCredit';
 
-const App = () => {
+function App() {
     return (
-        <div>
-            <h1>Business Plan Application</h1>
-            <BusinessPlanForm />
-            <BusinessPlanList />
-            {/* Render other components as needed */}
-        </div>
+        <Router>
+            <div className="App">
+                <h1>Business Plan Application</h1>
+                <Routes>
+                    <Route path="/create-business-plan" element={<BusinessPlanForm />} />
+                    <Route path="/business-plans" element={<BusinessPlanList />} />
+                    <Route path="/legal-business-formation" element={<LegalBusinessFormation />} />
+                    <Route path="/ein-number" element={<EINNumber />} />
+                    <Route path="/business-banking" element={<BusinessBanking />} />
+                    <Route path="/virtual-office" element={<VirtualOffice />} />
+                    <Route path="/business-phone-number" element={<BusinessPhoneNumber />} />
+                    <Route path="/business-website" element={<BusinessWebsite />} />
+                    <Route path="/duns-number" element={<DunsNumber />} />
+                    <Route path="/vendor-credit" element={<VendorCredit />} />
+                </Routes>
+            </div>
+        </Router>
     );
-};
+}
 
 export default App;
